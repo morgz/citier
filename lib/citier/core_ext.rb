2bfdef5a7d1f97882a59f78d709492936e0df57c
Daniel Morgan <danmorgz@gmail.com>
2012-03-03 22:50:00 +0000

    previously implementation using ::writable didnt do the job properly. It didnt clear the root class, so adding fields to the root of the inheritence buggered things up. well deleting actually, thats how I noticed this. Try and delete something from your root class.. now it should all be dandy

--- lib/citier/core_ext.rb
+++ lib/citier/core_ext.rb
@@ -35,10 +35,13 @@ class ActiveRecord::Base
 end
 
 def create_citier_view(theclass)  #function for creating views for migrations 
+  
+  return unless theclass.acts_as_citier? #Security in case we call this on a non-citier model
+  
   # flush any column info in memory
   # Loops through and stops once we've cleaned up to our root class.
-  # We MUST user Writable as that is the place where changes might reside!
-  reset_class = theclass::Writable 
+  reset_class = theclass
+  
   until reset_class == ActiveRecord::Base
     citier_debug("Resetting column information on #{reset_class}")
     reset_class.reset_column_information
